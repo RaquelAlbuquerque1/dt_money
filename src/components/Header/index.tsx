85038e8514b9051560782efd8d76377739843ec7
RaquelAlbuquerque1 <albuquerqueraquel396@gmail.com>
2021-08-06 13:57:09 -0300

    add types and events in the component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,17 @@
 import Logo from '../../assets/logo.svg'
 import { Container, Content } from './style'
+ interface HeaderProps{
+     onOpenNewTransactionModal: () => void;
+ }   
 
-export function Header() {
+export function Header({onOpenNewTransactionModal}: HeaderProps) {
+   
     return (
         <Container>
             <Content>
                 <img src={Logo} alt="dt money" />
-                <button type="button">Nova Transação</button>
+
+                <button type="button" onClick={onOpenNewTransactionModal}>Nova Transação</button>
             </Content>
         </Container>
     )
