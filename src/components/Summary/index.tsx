9bd32b19fa8bd248201e4814f4630f9fe9519d62
RaquelAlbuquerque1 <albuquerqueraquel396@gmail.com>
2021-08-06 13:58:34 -0300

    create the Summary component

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,32 +1,62 @@
 import { Container } from "./style";
+import { useTransactions } from "../../hooks/useTransactions";
 import income from '../../assets/income.svg'
 import outcome from '../../assets/outcome.svg'
 import total from '../../assets/total.svg'
 
-export function Summary(){
-    return(
-       <Container>
-           <div>
-               <header>
-                   <p>Entradas</p>
-                   <img src={income} alt="Entradas" />
-               </header>
-               <strong>R$1.000,00</strong>
-           </div>
-           <div>
-               <header>
-                   <p>Saídas</p>
-                   <img src={outcome} alt="Saídas" />
-               </header>
-               <strong> - R$500,00</strong>
-           </div>
-           <div className='highlight-background'>
-               <header>
-                   <p>Total</p>
-                   <img src={total} alt="Total" />
-               </header>
-               <strong>R$500,00</strong>
-           </div>
-       </Container>
+export function Summary() {
+
+    const { transactions } = useTransactions();
+
+    const summary = transactions.reduce((acc, transaction) => {
+        if (transaction.type === 'deposit') {
+            acc.deposits += transaction.amount;
+            acc.total += transaction.amount;
+        } else {
+            acc.withdraw += transaction.amount;
+            acc.total -= transaction.amount;
+        }
+        return acc;
+    },
+        {
+            deposits: 0,
+            withdraw: 0,
+            total: 0,
+        })
+
+    return (
+        <Container>
+            <div>
+                <header>
+                    <p>Entradas</p>
+                    <img src={income} alt="Entradas" />
+                </header>
+                <strong>
+                    {new Intl.NumberFormat('pt-BR', {
+                        style: 'currency',
+                        currency: 'BRL',
+                    }).format(summary.deposits)}</strong>
+            </div>
+            <div>
+                <header>
+                    <p>Saídas</p>
+                    <img src={outcome} alt="Saídas" />
+                </header>
+                <strong> - {new Intl.NumberFormat('pt-BR', {
+                    style: 'currency',
+                    currency: 'BRL',
+                }).format(summary.withdraw)}</strong>
+            </div>
+            <div className='highlight-background'>
+                <header>
+                    <p>Total</p>
+                    <img src={total} alt="Total" />
+                </header>
+                <strong>{new Intl.NumberFormat('pt-BR', {
+                    style: 'currency',
+                    currency: 'BRL',
+                }).format(summary.total)}</strong>
+            </div>
+        </Container>
     )
 }
